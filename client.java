1e4f97b01e48b4cad46e71704d2d81653e0a4b99
spydormunkay <cbutron1996@gmail.com>
2017-02-28 15:12:56 -0500

    Update client.java

--- client.java
+++ client.java
@@ -1,105 +1,35 @@
 /**
- * Created by christianbutron on 2/27/17.
+ * Created by christianbutron on 2/26/17.
  */
-import java.io.*;
 import java.net.*;
-import java.util.*;
+import java.io.*;
 
-public class Server {
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-    private static final int portNumber = 1337;
-
-    private static ArrayList<clientRunnable> clientThread;
-
+public class Client {
+    private static Socket server;
+    private static GUI gui;
+    private static PrintWriter pout;
+    private static BufferedReader bin;
 
     public static void main(String[] args) throws Exception {
-        serverSocket = new ServerSocket(portNumber);
-        clientThread = new ArrayList<clientRunnable>();
+        server = new Socket("127.0.0.1", 1337);
+        gui = new GUI(server);
+        OutputStream out = server.getOutputStream();
+        pout = new PrintWriter(out, true);
+        InputStream in = server.getInputStream();
+        bin = new BufferedReader(new InputStreamReader(in));
 
         while(true) {
-            clientSocket = serverSocket.accept();
-            clientRunnable curr = new clientRunnable(clientSocket);
-            clientThread.add(curr);
-            curr.start();
-        }
-    }
-
-    //public static class clientRunnable implements Runnable {
-    public static class clientRunnable extends Thread {
-        private Socket clientSocket;
-        private BufferedReader bin;
-        private PrintWriter pout;
-        private String name;
-        private Client client;
-
-        public clientRunnable(Socket clientSocket) throws Exception {
-            this.clientSocket = clientSocket;
-
-            InputStream in = clientSocket.getInputStream();
-            bin = new BufferedReader(new InputStreamReader(in));
-            OutputStream out = clientSocket.getOutputStream();
-            pout = new PrintWriter(out, true);
-        }
-
-        public void run() {
-            try {
-                while (true) {
-                    pout.println("Enter your name.");
-                    name = bin.readLine();
-                    break;
-                }
-
-                pout.println("Welcome to the chatroom, " + name + ".");
-                pout.println("To leave enter /quit in a new line.");
-                synchronized (this) {
-                    for (int i = 0; i < clientThread.size(); i++) {
-                        if (clientThread.get(i) != this) {
-                            clientThread.get(i).pout.println("*** " + name + " entered the chat room! ***");
-                        }
-                    }
-                }
-
-                while (true) {
-                    String line = bin.readLine();
-                    if (line.startsWith("/quit")) {
-                        break;
-                    }
-                    synchronized (this) {
-                        for (int i = 0; i < clientThread.size(); i++) {
-                            if (clientThread.get(i) != this) {
-                                clientThread.get(i).pout.println("<" + name + "> " + line);
-                            }
-                        }
-                    }
-                    pout.println("<" + name + "> " + line);
-                }
-
-                synchronized (this) {
-                    for (int i = 0; i < clientThread.size(); i++) {
-                        if (clientThread.get(i) != this) {
-                            clientThread.get(i).pout.println("*** The user " + name + " is leaving the chat room. ***");
-                        }
-                    }
-                }
-                pout.println("*** Bye " + name + " ***");
-                pout.println("/quit");
-
-                synchronized (this) {
-                    for (int i = 0; i < clientThread.size(); i++) {
-                        if (clientThread.get(i) == this) {
-                            clientThread.remove(i);
-                        }
-                    }
-                }
-
-                bin.close();
-                pout.close();
-                clientSocket.close();
-
-            } catch(Exception ex) {
-
+            String response = bin.readLine();
+            if(response.startsWith("/quit")) {
+                System.exit(0);
+                break;
             }
+            gui.send(response);
         }
+
+        bin.close();
+        pout.close();
+        server.close();
     }
+
 }
